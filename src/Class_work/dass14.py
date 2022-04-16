1478a38352ae71a410de0acbd845e56c4ff4d06b
Anvar Ermatov <1999ermatov@gmail.com>
2022-04-16 16:36:25 -0400

    index of the string

--- src/Class_work/dass14.py
+++ src/Class_work/dass14.py
@@ -1,3 +1,4 @@
+#index of the string
 city = 'Boston'
 index = 0
 
@@ -5,3 +6,13 @@ while index < len(city):
     print(city[index])
     index += 1
   
+"""
+def main():
+    first_name = input('Enter 1st name')
+    second_name = input('Enter 2nd name')
+
+    email = first_name[0] + second_name + '@su.suffolk.edu'
+
+    print(email)
+main()
+"""
