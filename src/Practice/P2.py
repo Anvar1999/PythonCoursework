8cb64390c09b873a9527397a42398426a4140652
Anvar Ermatov <1999ermatov@gmail.com>
2022-05-09 14:46:25 -0400

    Practice2

---
+++ src/Practice/P2.py
@@ -0,0 +1,98 @@
+#value = ("HELLO!, Hello")
+#value2 = str.lower(value)
+#print(value,value2)
+#value3 = str.upper(value4)
+#print(value3)
+
+
+
+
+#num1 = int(input("enter num1: "))
+#num2 = int(input("enter num2: "))
+#if num1 > num2:
+#    print(num2,num1)
+
+#else:
+#    print(num1,num2)
+
+
+
+
+
+#num3 = int(input("enter under num 20: "))
+#if num3 >= 20:
+#    print("Too high")
+#else:
+#    print('Thank you')
+
+
+
+
+#num4 = int(input('Enter num between 10 and 20'))
+#if num4 > 10 and num4 < 20:
+#    print('Thank you')
+#else:
+#    print('incorrect')
+
+
+
+#fav_color = input('enter your favorite color: ')
+#if fav_color == 'Red' or fav_color == 'RED' or fav_color == 'red':
+#    print('I like red too ')
+#else:
+#    print('I dont like the', fav_color, 'color, i prefer red')
+
+
+
+
+#rain = input('Is it raining right now? ')
+#rain = str.lower(rain)
+
+#if rain == 'yes':
+#    wind = input('Is it windy? answer yes or no ')
+#    wind = str.lower(wind)
+#    if wind == 'yes':
+#        print('it is too windy for an umbrella')
+#    else: 
+#        print('take an umbrella')
+#else:
+#    print('enjoy your day')
+
+
+
+#user_age = int(input('Enter your age: '))
+#if user_age >= 18:
+#    print('You can vote')
+
+#elif user_age == 17:
+#    print('You can learn to drive')
+
+#elif user_age == 16:
+#    print('You can buy a lottery ticket')
+
+#else:
+#    print('You can go Trick-or-Treating')
+
+
+
+
+#number = int(input('enter a number '))
+#if number < 10:
+#    print('Too low')
+#elif number >= 10 and number <= 20:
+#    print('Correct')
+#else: 
+#    print("Too high")
+
+
+
+#enter = int(input('input numbers 1,2 or 3: '))
+#if enter == 1: 
+#    print('Thank you')
+#elif enter == 2:
+#    print('Well done!')
+#elif enter == 3: 
+#    print('Correct!')
+#else:
+#    print('Error message')
+
