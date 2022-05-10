d6a63221ee975cbf1c65aa3bad15fa9192e2f2b4
Anvar Ermatov <1999ermatov@gmail.com>
2022-05-10 19:02:14 -0400

    Practice 5

---
+++ src/Practice/P5.py
@@ -0,0 +1,105 @@
+# for i in range(1,3):
+#     print(i)
+#     for j in range(2,4):
+#         print(j, end = '')
+#     print()
+
+
+# for i in 'word':
+#     print(i, end = "") 
+
+
+
+# name = input("enter your name: ")
+# number = int(input("How many times you want your name to be displayed? "))
+# for i in range(1, number + 1):
+
+#     print(name)
+
+
+
+# name = input("enter your name ")
+# for i in name:
+#     print(i, end = " ")
+
+
+# name = input("enter your name ")
+# number = int(input("Enter the number"))
+# for i in range(1, number + 1):
+#     for j in name:
+#         print(j)
+    
+
+
+# number = int(input("enter numbers between 1 and 12"))
+# for i in range(1, 13):
+#     ans = i * number
+#     print(i, "*", number, "=", ans)
+
+
+# number = int(input("enter number below 50: "))
+# for i in range(50, number - 1, -1):
+#     print(i)
+
+
+
+
+# name = input("Enter your name")
+# number = int(input("Enter your number"))
+# if number < 10: 
+#     for i in range(1, number + 1):
+#         print(name)
+# else:
+#     for i in range(1, 4):
+#         print("Too high")
+
+
+
+# total = 0
+
+# for i in range(1, 6):
+#     number = int(input("enter "))
+#     included = input("Do you want this number included to total  yes or no? ")
+#     if included == 'yes':
+#         total += number
+#     elif included == 'no':
+#         print("This number is not included to total") 
+    
+# print("total of collected numbers:",total)
+
+
+# direction = input("which direction you want to count, up or down?")
+# if direction == 'up':
+#     top = int(input("Enter top number: "))
+
+#     for i in range(1, top + 1):
+#         print(i)
+# elif direction == 'down':
+#     below = int(input('Enter number below 20: '))
+
+#     for i in range(20, below - 1, -1):
+#         print(i)
+# else: 
+#     print("I don't understand")
+
+
+
+
+# people = int(input("How many people do you want to invite? "))
+# if people < 10:
+#     for i in range(1, people + 1):
+#         names = input("Let me know the names ")
+#         print(names,"has been invited")
+# else:
+#     print("Too many people")
+
+
+
+
+
+
+
+
+
+
+
