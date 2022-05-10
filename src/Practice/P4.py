2da0410f20a69f9cd9a52a7c8f1bb97c51c4d336
Anvar Ermatov <1999ermatov@gmail.com>
2022-05-09 22:20:36 -0400

    Practice 4

---
+++ src/Practice/P4.py
@@ -0,0 +1,85 @@
+#Math
+import math
+
+# num = 500 
+
+# print(math.sqrt(num))
+
+# print(round(num,2))
+
+
+
+
+# num = float(input("enter a number"))
+# num *= 2
+# print(num)
+# print("%.2f" % num)
+
+
+
+# num = int(input("Enter number over 500 "))
+# num = math.sqrt(num)
+# print("numbers %.2f" % num)
+
+
+# num = 2
+# print(round(math.pi, 5))
+
+
+
+# radius = int(input("Whats the radius of the circle"))
+# area = math.pi*(radius**2)
+# print(area)
+
+
+
+
+# radius = int(input("enter the radius"))
+# cylinder = int(input("input the depth of cylinder"))
+# area = math.pi*(radius**2)
+# total_volume = area * cylinder
+# print(round(total_volume, 3))
+
+
+# num1 = int(input("enter num1"))
+# num2 = int(input("enter num2"))
+# cal = num1//num2 
+# reminder = num1%num2 
+# print(num1, ' divided by ', num2, ' is ',cal,' with ',reminder,' remaining')
+
+
+
+# print("1) Square")
+# print("2) Triangle")
+
+# num = int(input("Enter a number: "))
+# if num == 1:
+#     length = int(input("whats the length one of its sides"))
+#     area = length**2
+#     print(area)
+# elif num == 2:
+#     base = int(input("enter the base triangle"))
+#     height = int(input("enter the height of the triangle"))
+#     area = (height * base)/2
+#     print(area)
+# else:
+#     print("Error")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
