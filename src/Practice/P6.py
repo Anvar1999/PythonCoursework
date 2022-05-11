50dcf3c9dcc882f6cf9ae8bb43592ad80d239af0
Anvar Ermatov <1999ermatov@gmail.com>
2022-05-10 21:42:56 -0400

    Practice 6

---
+++ src/Practice/P6.py
@@ -0,0 +1,90 @@
+# total = 0 
+# while total <= 50:
+#     number = int(input("Enter a number"))
+#     total += number 
+#     print("The Total is", total)
+
+
+
+# number = 0
+# while number <= 5:
+#     number = int(input("Enter a number"))
+
+# print("The last time you entered", number)
+
+
+# number = int(input("Enter a number"))
+# total = number
+# i = 'yes'
+# while i == 'yes':
+#     number1 = int(input("Enter another number"))
+#     total = total + number1
+#     i = input('Do you want to continue? enter yes or no')
+# print("The total is", total)
+
+
+
+# i = 'yes'
+# count = 0
+# while i == 'yes':
+#     somebody = input("Enter the name of somebody:")
+#     print("You have invited %s" % somebody)
+#     count = count + 1
+#     i = input("Do you want to invite somebody else:")
+    
+
+# print("You have invited", count, "people")
+
+
+
+
+# compnum = 50
+# ask = int(input("Enter a number"))
+# count = 1
+# while ask != compnum:
+#     count = count + 1
+#     if ask > compnum: 
+#         print("too high")
+#     else:
+#         print("too low")
+    
+#     ask = int(input("Whats your next guess"))
+
+# print("Well done, you took %s" % count,"attemps")
+
+
+
+
+
+# num = int(input("enter number between 10 and 20"))
+# while num < 10 or num > 20: 
+#     if num < 10:
+#         print("too low")
+#         num = int(input("try again"))
+#     elif num > 20:
+#         print("too high")
+#         num = int(input("try again"))
+
+# print("Thank you")
+
+
+
+# num = 10 
+# while num != 0:
+
+#     print("There are", num,"green bottles hanging on the wall", num,"green bottles hanging on the wall")
+#     num = num - 1
+#     print("if 1 green bottle should accidentally fall")
+#     answer = int(input("How many bottles will be hanging on the wall? "))
+#     if num == answer:
+#         print("There will be",num,"green bottles hanging on the wall")
+#     else:
+#         while num != answer:
+#             answer = int(input("No try again"))
+
+# print("There are no more green bottles hanging on the wall")
+
+
+
+
+
