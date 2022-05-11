456df6ee4874e43a49758bdbf14fe70e20ad41bb
Anvar Ermatov <1999ermatov@gmail.com>
2022-05-11 19:42:38 -0400

    Practice 9

---
+++ src/Practice/P8.py
@@ -0,0 +1,193 @@
+import turtle 
+import random
+# src = turtle.Screen()
+# turtle.shape("square")
+
+# src.exitonclick()
+
+
+# src = turtle.Screen()
+# turtle.shape("triangle")
+
+# src.exitonclick()
+
+
+
+# src = turtle.Screen()
+# turtle.shape("circle")
+
+# src.exitonclick()
+
+
+
+
+
+# turtle.color("Black","Yellow")
+# turtle.begin_fill()
+# for i in range(0,4):
+#     turtle.forward(70)
+#     turtle.right(90)
+# turtle.penup()
+# turtle.end_fill()
+# turtle.forward(100)
+
+
+# turtle.pendown()
+# turtle.color("Black","Red")
+# turtle.begin_fill()
+# for i in range(0,4):
+#     turtle.forward(70)
+#     turtle.right(90)
+# turtle.penup()
+# turtle.end_fill()
+# turtle.forward(100)
+
+# turtle.pendown()
+# turtle.color("Black","Green")
+# turtle.begin_fill()
+# for i in range(0,4):
+#     turtle.forward(70)
+#     turtle.right(90)
+# turtle.end_fill()
+
+# turtle.exitonclick()
+
+
+
+# turtle.pendown()
+# for i in range(0,5):
+    
+#     turtle.right(144)
+#     turtle.forward(100)
+
+
+# turtle.exitonclick()
+
+
+# turtle.pendown()
+# for i in range(0,1):
+    
+#     turtle.right(90)
+#     turtle.forward(120)
+#     turtle.penup()
+#     turtle.left(90)
+#     turtle.forward(120)
+    
+#     turtle.left(90)
+#     turtle.forward(120)
+#     turtle.pendown()
+#     turtle.right(90)
+#     turtle.forward(100)
+#     turtle.right(90)
+#     turtle.forward(60)
+#     turtle.right(90)
+#     turtle.forward(100)
+#     turtle.left(90)
+#     turtle.forward(60)
+#     turtle.left(90)
+#     turtle.forward(100)
+#     turtle.penup()
+#     turtle.forward(100)
+#     turtle.left(90)
+#     turtle.forward(120)
+#     turtle.pendown()
+#     turtle.right(90)
+#     turtle.forward(100)
+#     turtle.right(90)
+#     turtle.forward(60)
+#     turtle.right(90)
+#     turtle.forward(70)
+#     turtle.penup()
+#     turtle.right(180)
+#     turtle.forward(70)
+#     turtle.pendown()
+#     turtle.right(90)
+#     turtle.forward(60)
+#     turtle.right(90)
+#     turtle.forward(100)
+# turtle.hideturtle()
+# turtle.exitonclick()
+
+
+
+
+# for i in range(0,6):
+
+
+#     turtle.begin_fill()
+#     turtle.color(random.choice(["Red","Blue","Yellow","Orange","Black","Green"]))
+#     turtle.right(60)
+#     turtle.forward(100)
+# turtle.exitonclick()
+
+
+
+
+# for i in range(0,6):
+#     turtle.left(60)
+#     turtle.forward(100)
+# for i in range(0,6):
+#     turtle.right(60)
+#     turtle.forward(100)
+# turtle.right(60)
+# for i in range(0,6):
+#     turtle.right(60)
+#     turtle.forward(100)
+# turtle.left(60)
+# turtle.left(60)
+# for i in range(0,6):
+#     turtle.right(60)
+#     turtle.forward(100)
+# turtle.left(60)
+# for i in range(0,6):
+#     turtle.right(60)
+#     turtle.forward(100)
+# turtle.left(60)
+# for i in range(0,6):
+#     turtle.right(60)
+#     turtle.forward(100)
+
+# turtle.left(90)
+# for i in range(0,6):
+#     turtle.left(60)
+#     turtle.forward(100)
+# for i in range(0,6):
+#     turtle.right(60)
+#     turtle.forward(100)
+# turtle.left(60)
+# for i in range(0,6):
+#     turtle.right(60)
+#     turtle.forward(100)
+# turtle.left(60)
+# for i in range(0,6):
+#     turtle.right(60)
+#     turtle.forward(100)
+# turtle.left(90)
+# turtle.left(90)
+# for i in range(0,6):
+#     turtle.right(60)
+#     turtle.forward(100)
+# turtle.right(60)
+# turtle.right(60)
+# for i in range(0,6):
+#     turtle.right(60)
+#     turtle.forward(100)
+    
+
+
+# for x in range(0,10):
+#     for i in range(0,8):
+#         turtle.forward(50)
+#         turtle.right(45)
+#     turtle.right(36)
+
+# turtle.hideturtle()
+# turtle.exitonclick()
+
+
+
+
+
+
+
+
